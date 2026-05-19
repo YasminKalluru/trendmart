3417175430185ab03454cfbfc2bbc85b28356d92
Yasmin <122411510213@apollouniversity.edu.in>
2026-05-19 17:28:20 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/yasmin/trendmart/TrendmartApplication.java
@@ -0,0 +1,13 @@
+package com.yasmin.trendmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendmartApplication.class, args);
+	}
+
+}
